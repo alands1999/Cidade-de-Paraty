4c2a5cb7ea9b5947546a8c38173a5ade1df07224
alands1999 <alands1999@gmail.com>
2025-03-10 23:12:15 -0300

    Criação da estrutura de pastas e navegação

--- cidadeParaty/App.js
+++ cidadeParaty/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import TelaInicial from './Componentes/TelaInicial';
+import TelaRestaurantes from './Componentes/TelaRestaurantes';
+import TelaHospedagem from './Componentes/TelaHospedagem';
+import TelaPasseios from './Componentes/TelaPasseios';
+
+const Stack = createStackNavigator();
+
+export default function App (){
+  return(
+      <NavigationContainer>
+          <Stack.Navigator>
+
+            <Stack.Screen name="Home" component={TelaInicial} />
+            <Stack.Screen name="Restaurantes" component={TelaRestaurantes} />
+            <Stack.Screen name="Hospedagem" component={TelaHospedagem} />
+            <Stack.Screen name="Passeios" component={TelaPasseios} />
+
+          </Stack.Navigator>
+      </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
