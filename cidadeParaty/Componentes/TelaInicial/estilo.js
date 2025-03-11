60cddf27fed57b780786b02ae06031cb14e67824
alands1999 <alands1999@gmail.com>
2025-03-10 23:20:27 -0300

    Criação dos componentes da tela inicial

---
+++ cidadeParaty/Componentes/TelaInicial/estilo.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    img: {
+        height: 200,
+    }
+})
+
+export default estilos;
--- cidadeParaty/Componentes/TelaInicial/index.js
+++ cidadeParaty/Componentes/TelaInicial/index.js
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Button } from "react-native";
+import estilos from './estilo';
 
-export default function TelaInicial(){
+import Paraty from '../../assets/01.png'
+
+export default function TelaInicial(props){
     return(
-        <View>
-            <Text>Tela Inicial</Text>
+        <View style={estilos.container}>
+            <View style={estilos.card}>
+                
+                <Text style={estilos.titulo}>Paraty</Text>
+                <Text>Saiba o que visitar em paraty.</Text>
+                <Image source={Paraty} style={estilos.img} />
+                <Button title="Ver Restaurantes" onPress={ () => {props.navigation.navigate('Restaurantes')}} />
+                <Button title="Ver Passeios" onPress={ () => {props.navigation.navigate('Passeios')}}  />
+                <Button title="Ver Hospedagem" onPress={ () => {props.navigation.navigate('Hospedagem')}}  />
+
+            </View>
         </View>
     )
 }
