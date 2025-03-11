708465d94c6bd10eb5cdcfd74a521b1ea6af7bf8
alands1999 <alands1999@gmail.com>
2025-03-10 23:33:37 -0300

    Estilização da tela inicial

--- cidadeParaty/Componentes/TelaInicial/estilo.js
+++ cidadeParaty/Componentes/TelaInicial/estilo.js
@@ -5,10 +5,37 @@ const estilos = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#e1e3e4'
     },
     img: {
+        width: 300,
         height: 200,
-    }
+        marginBottom: 20,
+    },
+    card: {
+        width: 320,
+        backgroundColor: 'white',
+        padding: 10,
+        borderColor: "#979797",
+        borderWidth: 1,
+        borderRadius: 5,
+
+    },
+    titulo: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+
+    },
+    texto: {
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    box: {
+        gap: 10,
+    },
+    
 })
 
 export default estilos;
--- cidadeParaty/Componentes/TelaInicial/index.js
+++ cidadeParaty/Componentes/TelaInicial/index.js
@@ -7,13 +7,16 @@ export default function TelaInicial(props){
     return(
         <View style={estilos.container}>
             <View style={estilos.card}>
-                
+
                 <Text style={estilos.titulo}>Paraty</Text>
-                <Text>Saiba o que visitar em paraty.</Text>
+                <Text style={estilos.texto}>Saiba o que visitar em paraty.</Text>
                 <Image source={Paraty} style={estilos.img} />
-                <Button title="Ver Restaurantes" onPress={ () => {props.navigation.navigate('Restaurantes')}} />
-                <Button title="Ver Passeios" onPress={ () => {props.navigation.navigate('Passeios')}}  />
-                <Button title="Ver Hospedagem" onPress={ () => {props.navigation.navigate('Hospedagem')}}  />
+
+                <View style={estilos.box}>
+                    <Button color="#372d00" title="Ver Restaurantes" onPress={ () => {props.navigation.navigate('Restaurantes')}} />
+                    <Button color="#372d00" title="Ver Passeios" onPress={ () => {props.navigation.navigate('Passeios')}}  />
+                    <Button color="#372d00" title="Ver Hospedagem" onPress={ () => {props.navigation.navigate('Hospedagem')}}  />
+                </View>
 
             </View>
         </View>
